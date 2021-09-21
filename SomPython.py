07c08df790cadcc6397951edc42442417b2e31a6
IanSchaafsma <33319@ma-web.nl>
2021-09-21 12:10:09 +0200

    second commit

---
+++ SomPython.py
@@ -0,0 +1,23 @@
+som1 = 5678-4321
+som2 = 93*13-99
+print(som1 + som2)
+
+stringA = "Mafkees!"
+print(stringA[3:7])
+
+a = "Firetruck"
+print((a[0:1]) + a[6:9])
+
+b = "Dabani"
+print((b[0:2]) + b[4:6])
+
+print("What's your favorite number")
+z = input()
+print("your number is " + z )
+
+print("put in your first number")
+n = input()
+print("put in your second number")
+m = input()
+b = int(n)+int(m)
+print(b)  
