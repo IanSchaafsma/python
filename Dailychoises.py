3d221fa0faa9cbfeb02c4342ab390723fff5446d
Ian <33319@ma-web.nl>
2021-09-24 10:33:31 +0200

    Dailychoises.pt2

--- Dailychoises.py
+++ Dailychoises.py
@@ -3,7 +3,7 @@ while True:
     Start = input("Wil je beginnen ja/nee ")
     if Start == 'ja':
      print("Veel plezier!")
-    if Start == 'nee':
+    elif Start == 'nee':
      print("Ok doei")
      break     
     
@@ -11,21 +11,21 @@ while True:
     Vraag1 = input("A = je snoozed je wekker nog 5 minuutjes B = ja staat op ")
     if Vraag1 == 'A':
      print("je word wakker en shit je bent nu al dik een uur te laat")
-    if Vraag1 == 'B':
+    elif Vraag1 == 'B':
      print("je pakt je spullen en stapt de deur uit")   
     
     print("pak je je fiets of ga je lopen")
     Vraag2 = input("fiets/lopen ")
     if Vraag2 == 'fiets':
      print("je pakt je fiets en gaat naar het station")
-    if Vraag2 == 'lopen':
+    elif Vraag2 == 'lopen':
      print("je begint met lopen naar het station")   
 
     print("je staat op het station neem je koffie of thee")
     Vraag3 = input("koffie/thee ")
     if Vraag3 == 'koffie':
      print("mhm wat een lekker bakkie koffie")
-    if Vraag3 == 'thee':
+    elif Vraag3 == 'thee':
      print("mhm wat een lekker koppie thee")   
 
      print("je pakt je trein ga je staan of zitten")
@@ -33,6 +33,8 @@ while True:
     if Vraag4 == 'staan':
      print("pff wel vermoeiden hoor")
      break
-    if Vraag4 == 'zitten':
+    elif Vraag4 == 'zitten':
      print("even lekker zitten")
      break 
+    
+    else: print("je bent klaar")
