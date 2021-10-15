50504d3a6ffa2884b114b3a26d4195e10718cfe5
Ian <33319@ma-web.nl>
2021-10-15 11:17:52 +0200

    L6.2

--- PYTB1L6TransportItems.py
+++ PYTB1L6TransportItems.py
@@ -3,25 +3,27 @@ import time
 
 while True:
     factory = ["auto"]
-    distribution = ["auto"]
-    shop = ["auto"]
+    distribution = []
+    shop = []
 
     print("fabriek:", factory )
-    factory.pop(0)
+    popped_from_factory = factory.pop(0)
+    distribution.append(popped_from_factory)
     time.sleep(1)
     print("Fabriek:", factory)
 
     time.sleep(1)
 
     print("distribution:", distribution )
-    distribution.pop(0)
+    popped_from_distribution = distribution.pop(0)
+    shop.append(popped_from_distribution)
     time.sleep(1)
     print("distribution:", distribution)
 
     time.sleep(1)
 
     print("shop:", shop )
-    shop.pop(0)
+    popped_fromshop = shop.pop(0)
     time.sleep(1)
     print("shop:", shop)
 
